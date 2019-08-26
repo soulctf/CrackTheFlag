ad648b2c9695aad05a2e34c133c372b28c25588b
Soul <soulctf@gmail.com>
2019-08-26 18:24:09 -0400

    Added Caesar Cipher and rot47

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,64 @@
 #[cfg(test)]
 mod tests {
-    use crate::crypto::caesar;
+    use crate::crypto;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
-    
+    #[test]
     fn crypto_test(){
         // Tests to see if we get the desired output
-        assert_eq!(caesar("Hello", 5), "Mjqqt")
+        assert_eq!(crypto::caesar("Test123")[19], "Mxlm123");
+        assert_eq!(crypto::rot47("4-'3evh?'c)7%t#e-r,g6u#.9uv#%tg2v#7g'w6gA")[46], "picoCTF{cAesaR_CiPhErS_juST_aREnT_sEcUrE}");
     }
 }
 
 mod crypto {
     
-    use std::io;
-
     // All crypto functions will go here
-    pub fn caesar(data: &str, shift: u8) -> String {
+
+    //ROT FUNCTIONS - SHOULD ABSTRACT FOR CUSTOM ALPHABET
+    pub fn caesar(cipher_text: &str) -> Vec<String> {
         
-        let mut characters = data.to_string();
-        for i in characters.chars() {
-            print!("{:?}", i)
+        let mut plain_text: Vec<String> = Vec::new(); 
+        for shift in 0..26 {
+            let mut temp = String::new();
+            for c in cipher_text.chars() {
+                if c.is_alphabetic() {
+                    if c.is_lowercase() {
+                        temp.push(((c as u8 - 97 + shift)%26 + 97) as char);
+                    } else {
+                        temp.push(((c as u8 - 65 + shift)%26 + 65) as char);
+                    }
+                } else {
+                    temp.push(c);
+                }
+            }
+            plain_text.push(temp);
         }
-        "Test".to_string()
+        plain_text
     }
 
-    fn rot47() {
+    pub fn rot47(cipher_text: &str) -> Vec<String> {
 
+        let mut plain_text: Vec<String> = Vec::new();
+        for offset in 0..93 {
+            let mut temp = String::new();
+            for c in cipher_text.chars() {
+                if c as u8 >= 33 && c as u8 <= 126 {
+                    temp.push((33 + ((c as u8 + offset) % 94)) as char)
+                } else {
+                    temp.push(c)
+                }
+            }
+            plain_text.push(temp);    
+        }
+        plain_text
     }
 
     fn morse(){
 
-    }
 
+    }
     
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,6 @@
 mod crypto {
     
-    use std::char;
-
-    // All crypto functions will go here
-    pub fn caesar(text: &str, shift: u8) -> String {
-        
-        let mut encrypted_text = String::new(); 
-        for c in text.chars() {
-            if c.is_alphabetic() {
-                if c.is_lowercase() {
-                    encrypted_text.push(((c as u8 - 97 + shift)%26 + 97) as char)
-                } else {
-                    encrypted_text.push(((c as u8 - 65 + shift)%26 + 65) as char)
-                }
-            } else {
-                encrypted_text.push(c)
-            }
-            
-        }
-        encrypted_text
-    }
+   
 
     fn rot47() {
 
@@ -33,13 +14,84 @@ mod crypto {
             v.push(i);
         }
         println!("{:?}", v);
+        use std::collections::HashMap;
 
+        // Takes in data and converts it to a String
+        let data = data.to_string();
+        // Splits string by a whitespace character
+        let msg = data.split(" ");
+        // Creates a preinitilaized hash table in the form of an array for morse code; basically a large static lookup table
+        let md: HashMap<String, char> = [
+            // Standard Morse Dictionary(key, value)
+            (".-".to_string(), 'A'),
+            ("-...".to_string(), 'B'),
+            ("-.-.".to_string(), 'C'),
+            ("-..".to_string(), 'D'),
+            (".".to_string(), 'E'),
+            ("..-.".to_string(), 'F'),
+            ("--.".to_string(), 'G'),
+            ("....".to_string(), 'H'),
+            ("..".to_string(), 'I'),
+            (".---".to_string(), 'J'),
+            ("-.-".to_string(), 'K'),
+            (".-..".to_string(), 'L'),
+            ("--".to_string(), 'M'),
+            ("-.".to_string(), 'N'),
+            ("---".to_string(), 'O'),
+            (".--.".to_string(), 'P'),
+            ("--.-".to_string(), 'Q'),
+            (".-.".to_string(), 'R'),
+            ("...".to_string(), 'S'),
+            ("-".to_string(), 'T'),
+            ("..-".to_string(), 'U'),
+            ("...-".to_string(), 'V'),
+            (".--".to_string(), 'W'),
+            ("-..-".to_string(), 'X'),
+            ("-.--".to_string(), 'Y'),
+            ("--..".to_string(), 'Z'),
+            ("-----".to_string(), '0'),
+            (".----".to_string(), '1'),
+            ("..---".to_string(), '2'),
+            ("...--".to_string(), '3'),
+            ("....-".to_string(), '4'),
+            (".....".to_string(), '5'),
+            ("-....".to_string(), '6'),
+            ("--...".to_string(), '7'),
+            ("---..".to_string(), '8'),
+            ("----.".to_string(), '9'),
+            (".-...".to_string(), '&'),
+            (".--.-.".to_string(), '@'),
+            ("-.--.-".to_string(), ')'), 
+            ("-.--.".to_string(), '('),
+            ("---...".to_string(), ':'),
+            ("--..--".to_string(), ','), 
+            ("-...-".to_string(), '='),
+            ("-.-.--".to_string(), '!'),
+            (".-.-.-".to_string(), '.'),
+            ("-....-".to_string(), '-'), 
+            (".-.-.".to_string(), '+'),
+            ("..--..".to_string(), '?'),
+            ("-..-.".to_string(), '/')
+        // Iterates, clones, and collects all values from the map so that they can be looped though
+        ].iter().cloned().collect();
+        // Creates and Empty String to store concatenated characters
+        let mut unencrypted = String::new();
+        // Loops through both the message and the map and compares each instance of morse to values within the map
+        for instance in msg {
+            for (key, value) in &md {
+                if instance == key {
+                    // Dereferences the value and pushes it onto a string
+                    unencrypted.push(*value);
+                }
+            }
+        }
+        // Prints out the final unencrypted string
+        println!("{}", unencrypted);
     }
 
     
 }
 
 fn main() {
-    println!("{:?}", crypto::caesar("ebiil tloia", 3));
-    println!("{:?}", crypto::morse("..../..-"));
+    //println!("{:?}", crypto::morse("..../..-"));
 }
